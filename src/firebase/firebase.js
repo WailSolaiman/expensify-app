73f6fd114cb0793e3261b2b9c29a4b6d7c614ee3
Wail Solaiman <waily2@hotmail.com>
2018-04-25 14:33:08 +0200

    Add dummy data to firebase DB

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,6 +13,73 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Wail Solaiman'
+const expenses = [{
+    description: 'Gas Bill',
+    note: 'Gas Bill April 2018',
+    amount: 4500,
+    createdAt: 1000
+},
+{
+    description: 'Rent',
+    note: 'Rent April 2018',
+    amount: 54500,
+    createdAt: 2000
+},
+{
+    description: 'Water Bill',
+    note: 'Water Bill April 2018',
+    amount: 7500,
+    createdAt: 1000
+}];
+
+expenses.map((expense) => {
+    database.ref('expenses').push(expense);
 });
+
+
+
+// database.ref().set({
+//     name: 'Wail Solaiman',
+//     age: 35,
+//     job: {
+//         title: 'Frontend Web Developer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Hamburg',
+//         country: 'Germany'
+//     }
+// });
+
+// const dataSubscribe = database.ref().on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(`${val.name} is working as ${val.job.title} at ${val.job.company}`);
+// }, (error) => {
+//     console.log('Error detected: ', error);
+// });
+
+// setTimeout(() => {
+//     database.ref('job/company').set('Amazon');
+// }, 3000);
+
+// setTimeout(() => {
+//     database.ref().off('value', dataSubscribe);
+// }, 5000);
+
+// setTimeout(() => {
+//     database.ref('location/city').set('Berlin');
+// }, 8000);
+
+// database.ref().update({
+//     name: 'Wail O. Solaiman',
+//     'job/company': 'Google'
+// });
+
+// database.ref('age')
+//     .remove()
+//     .then(() => {
+//         console.log('Age removed.')
+//     })
+//     .catch((error) => {
+//         console.log('Error: ', error);
+//     });
