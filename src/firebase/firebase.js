85f8adc361423ca7d7e98c35c57c758b01cfd656
Wail Solaiman <waily2@hotmail.com>
2018-04-30 12:27:47 +0200

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,40 +1,42 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBIXF_giKJbs1xSIErSpZmRaw33WUORC5Y",
-    authDomain: "expensify-274b1.firebaseapp.com",
-    databaseURL: "https://expensify-274b1.firebaseio.com",
-    projectId: "expensify-274b1",
-    storageBucket: "expensify-274b1.appspot.com",
-    messagingSenderId: "259767268937"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
-const expenses = [{
-    description: 'Gas Bill',
-    note: 'Gas Bill April 2018',
-    amount: 4500,
-    createdAt: 1000
-},
-{
-    description: 'Rent',
-    note: 'Rent April 2018',
-    amount: 54500,
-    createdAt: 2000
-},
-{
-    description: 'Water Bill',
-    note: 'Water Bill April 2018',
-    amount: 7500,
-    createdAt: 1000
-}];
+export { firebase, database as default };
 
-expenses.map((expense) => {
-    database.ref('expenses').push(expense);
-});
+// const expenses = [{
+//     description: 'Gas Bill',
+//     note: 'Gas Bill April 2018',
+//     amount: 4500,
+//     createdAt: 1000
+// },
+// {
+//     description: 'Rent',
+//     note: 'Rent April 2018',
+//     amount: 54500,
+//     createdAt: 2000
+// },
+// {
+//     description: 'Water Bill',
+//     note: 'Water Bill April 2018',
+//     amount: 7500,
+//     createdAt: 1000
+// }];
+
+// expenses.map((expense) => {
+//     database.ref('expenses').push(expense);
+// });
 
 
 
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,11 @@
 import Enzym from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
 
 Enzym.configure({
     adapter: new Adapter()
+});
+
+DotEnv.config({
+    path: '.env.test'
 });
