7bbfa87e0e4aaef7ca3d73e0f13b33e4ed53af2d
Wail Solaiman <waily2@hotmail.com>
2018-05-05 15:42:14 +0200

    Add expensify styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import selectExpensesTotal from '../selectors/expenses-total';
-import selectExpenses from '../selectors/expenses';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
-    let message = '';
-    if (expenseCount) {
-        const expenseWord = expenseCount === 1? 'expense' : 'expenses';
-        const formatedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
-        message = `Viewing ${expenseCount} ${expenseWord} totaling ${formatedExpensesTotal}`;
-    }  
-    return (
-        <div>
-            <h1>{message}</h1>
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+
+  return (
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-    const visibleExpenses = selectExpenses(state.expenses, state.filters);
-    return {
-        expenseCount: visibleExpenses.length,
-        expensesTotal: selectExpensesTotal(visibleExpenses)
-    };
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
+
+  return {
+    expenseCount: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal(visibleExpenses)
+  };
 };
 
 export default connect(mapStateToProps)(ExpensesSummary);
